ed764a51ba21f69e0cd4b8199fa340c369b7b4e5
Jesus Rosas <infinitdeveloperofficial@gmail.com>
2016-06-11 00:24:29 -0500

    generation of forms with form builder of Rails

---
+++ app/form_builders/bootstrap_form_builder.rb
@@ -0,0 +1,24 @@
+class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
+	delegate :content_tag, to: :@template
+	#Generador de metodos
+	[:text_field, :text_area, :url_field, :email_field, :number_field].each do |metodo|
+		define_method metodo do |name,*args|
+			options = args.extract_options!
+			if options.has_key?(:class)
+				options[:class] +="form-control"
+			else
+				options[:class] ="form-control"
+			end
+			content_tag :div, class:"form-group" do
+				content_tag(:label, name, name:name) + super(name,options)
+			end
+		end
+	end	
+
+	#Boton submit
+	def submit(*args)
+		content_tag :div do
+			super(*args,class:"btn btn-success")
+		end
+	end
+end
