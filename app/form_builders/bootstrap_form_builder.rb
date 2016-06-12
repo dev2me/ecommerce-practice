5c1abe16b92ef9f167909278a456631d93324662
Jesus Rosas <infinitdeveloperofficial@gmail.com>
2016-06-12 00:37:37 -0500

    finished forms builder and design of card of add_to_products

--- app/form_builders/bootstrap_form_builder.rb
+++ app/form_builders/bootstrap_form_builder.rb
@@ -4,17 +4,33 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
 	[:text_field, :text_area, :url_field, :email_field, :number_field].each do |metodo|
 		define_method metodo do |name,*args|
 			options = args.extract_options!
+			additional_clases = "form-control floating-label"
 			if options.has_key?(:class)
-				options[:class] +="form-control"
+				options[:class] +=additional_clases
 			else
-				options[:class] ="form-control"
+				options[:class] =additional_clases
 			end
+			options[:placeholder] = name.capitalize
 			content_tag :div, class:"form-group" do
-				content_tag(:label, name, name:name) + super(name,options)
+				super(name,options)
 			end
 		end
 	end	
-
+	#Errors
+	def errors
+		if object.errors.any?
+			content_tag :div, class:"red large-padding white-text" do
+				content_tag(:h2, "Errors") +
+				content_tag(:ul) do
+					lis = ""
+					object.errors.full_messages.each do |err|
+						lis += content_tag :li, err
+					end
+					lis.html_safe
+				end
+			end
+		end
+	end
 	#Boton submit
 	def submit(*args)
 		content_tag :div do
