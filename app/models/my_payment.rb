8cd7880494e3c02fd3149975d2768eb8bd8a9a8e
Jesus Rosas <infinitdeveloperofficial@gmail.com>
2016-06-14 16:31:32 -0500

    notes in models reference to schema

--- app/models/my_payment.rb
+++ app/models/my_payment.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: my_payments
+#
+#  id         :integer          not null, primary key
+#  email      :string
+#  ip         :string
+#  status     :string
+#  fee        :decimal(6, 2)
+#  paypal_id  :string
+#  total      :decimal(8, 2)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class MyPayment < ActiveRecord::Base
 end
--- spec/models/my_payment_spec.rb
+++ spec/models/my_payment_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: my_payments
+#
+#  id         :integer          not null, primary key
+#  email      :string
+#  ip         :string
+#  status     :string
+#  fee        :decimal(6, 2)
+#  paypal_id  :string
+#  total      :decimal(8, 2)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe MyPayment, type: :model do
